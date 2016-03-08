c375b11eb600814670bc39f8bffa8c5f089db702
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2016-03-08 15:58:43 +0100

    update oefeningen
    oef 6 info+ oplossing probleem voor mezelf opgelost

--- OEFENINGEN/3_jdbc/jdbc_db4free/src/main/java/be/pxl/jdbc/Oefening6.java
+++ OEFENINGEN/3_jdbc/jdbc_db4free/src/main/java/be/pxl/jdbc/Oefening6.java
@@ -11,16 +11,18 @@ public class Oefening6 {
         ){
             System.err.println("We hebben een connectie");
 
-            try(Statement statement = con.createStatement(); Statement statement2 = con.createStatement()){
+            try(	Statement statement = con.createStatement();
+            		Statement statement2 = con.createStatement())
+            {
                 con.setAutoCommit(false);
                 statement.executeUpdate("UPDATE bankrekening SET saldo = 800 WHERE rekeningnummer = 1");
-                if(true)
-                	throw new Exception("Oei, de data is nog niet gecommit en er loopt iets fout");
+               // if(true)
+               // 	throw new Exception("Oei, de data is nog niet gecommit en er loopt iets fout");
                 statement2.executeUpdate("UPDATE bankrekening SET saldo = 400 WHERE rekeningnummer = 2");
                 con.commit();
             } catch (Exception e){
             	//System.err.println(e); //--> De tekst van de error in de exception throw
-                System.err.println("De transactie is foutgelopen");
+                System.err.println("De transactie is foutgelopen");     
                 con.rollback();
             }
 
